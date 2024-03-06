7ae52408d02ac527aa874309ada83c1967bc6a34
patkarns <30379111+patkarns@users.noreply.github.com>
2024-03-06 05:36:42 -0800

    refactor [upvotes-list]: adjust styling of the upvotes component

--- src/components/upvote/Upvote.tsx
+++ src/components/upvote/Upvote.tsx
@@ -2,27 +2,22 @@ import { useState } from "react";
 
 import colors from "../../styles/utilities/colors.module.scss";
 import { ReactComponent as ArrowUpIcon } from "../../assets/arrow-up.svg";
+import "./Upvote.scss";
 
-function Upvote({isSelected, setIsSelected}) {
-
+function Upvote({ isSelected, setIsSelected }) {
   return (
-    <>
-      <div>
-        <button
-          style={{
-            backgroundColor: isSelected
-              ? colors.backgroundSelected
-              : colors.backgroundDefault,
-          }}
-          onClick={() => setIsSelected(!isSelected)}
-        >
-          {" "}
-          <ArrowUpIcon
-            fill={isSelected ? colors.selected : colors.default}
-          />{" "}
-        </button>
-      </div>
-    </>
+    <button
+      style={{
+        backgroundColor: isSelected
+          ? colors.backgroundSelected
+          : colors.backgroundDefault,
+      }}
+      className="UpvoteButton"
+      onClick={() => setIsSelected(!isSelected)}
+    >
+      {" "}
+      <ArrowUpIcon fill={isSelected ? colors.selected : colors.default} />{" "}
+    </button>
   );
 }
 
