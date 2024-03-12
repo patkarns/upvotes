0105cc3a6df8e822c25106c03ae32838f4f4e1b5
patkarns <30379111+patkarns@users.noreply.github.com>
2024-03-11 23:40:53 -0700

    add [upvotes-list]: add vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import svgr from "@svgr/rollup";
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+  },
 })
