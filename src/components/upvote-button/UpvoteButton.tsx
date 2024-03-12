9c4a20f0576e9ee5b4ca495604bc433d7c9b3c2b
patkarns <30379111+patkarns@users.noreply.github.com>
2024-03-11 19:08:50 -0700

    change [upvotes-list]: rename Upvote component to UpvoteButton

---
+++ src/components/upvote-button/UpvoteButton.tsx
@@ -0,0 +1,22 @@
+import colors from "../../styles/utilities/colors.module.scss";
+import { ReactComponent as ArrowUpIcon } from "../../assets/arrow-up.svg";
+import "./UpvoteButton.scss";
+
+function UpvoteButton({ isSelected, setIsSelected }) {
+  return (
+    <button
+      style={{
+        backgroundColor: isSelected
+          ? colors.backgroundSelected
+          : colors.backgroundDefault,
+      }}
+      className="UpvoteButton"
+      onClick={() => setIsSelected(!isSelected)}
+    >
+      {" "}
+      <ArrowUpIcon fill={isSelected ? colors.selected : colors.default} />{" "}
+    </button>
+  );
+}
+
+export default UpvoteButton;
